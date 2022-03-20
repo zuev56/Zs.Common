de72f7c15f293dba502d396e903fb8728ab91ee4
Sergei Zuev <zuev56@ro.ru>
2022-03-20 13:57:25 +0300

    Remove data abstractions and DbClientBase

--- src/Abstractions/Data/IItemsWithRawDataRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Zs.Common.Abstractions.Data;
-
-public interface IItemsWithRawDataRepository<TEntity, TKey> : IRepository<TEntity, TKey>
-    where TEntity : class, IDbEntityWithRawData<TEntity, TKey>
-{
-    Task<TKey> GetActualIdByRawDataHashAsync(TEntity item);
-}
