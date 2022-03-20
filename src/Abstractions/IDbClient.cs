46d2a71ee36013b2735c47058bfdd89f9a8209ed
Sergei Zuev <zuev56@ro.ru>
2022-03-20 17:32:32 +0300

    Return IDbClient

---
+++ src/Abstractions/IDbClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Zs.Common.Enums;
+
+namespace Zs.Common.Abstractions.Data;
+
+public interface IDbClient
+{
+    Task<string> GetQueryResultAsync(string sqlQuery);
+    Task<string> GetQueryResultAsync(string sqlQuery, QueryResultType resultType);
+}
