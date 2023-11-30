5fc6d18f79a487f60d06912059bb32403bf54270
Sergei Zuev <zuev56@ro.ru>
2023-11-30 21:11:54 +0300

    feat: add extension methods to convert double and long to TimeSpan

--- src/Extensions/TimeSpanExtensions.cs
+++ src/Extensions/TimeSpanExtensions.cs
@@ -5,12 +5,22 @@ namespace Zs.Common.Extensions;
 
 public static class TimeSpanExtensions
 {
+    public static TimeSpan Microseconds(this int value) => TimeSpan.FromMicroseconds(value);
     public static TimeSpan Milliseconds(this int value) => TimeSpan.FromMilliseconds(value);
     public static TimeSpan Seconds(this int value) => TimeSpan.FromSeconds(value);
     public static TimeSpan Minutes(this int value) => TimeSpan.FromMinutes(value);
     public static TimeSpan Hours(this int value) => TimeSpan.FromHours(value);
     public static TimeSpan Days(this int value) => TimeSpan.FromDays(value);
     public static TimeSpan Ticks(this int value) => TimeSpan.FromTicks(value);
+
+    public static TimeSpan Microseconds(this double value) => TimeSpan.FromMicroseconds(value);
+    public static TimeSpan Milliseconds(this double value) => TimeSpan.FromMilliseconds(value);
+    public static TimeSpan Seconds(this double value) => TimeSpan.FromSeconds(value);
+    public static TimeSpan Minutes(this double value) => TimeSpan.FromMinutes(value);
+    public static TimeSpan Hours(this double value) => TimeSpan.FromHours(value);
+    public static TimeSpan Days(this double value) => TimeSpan.FromDays(value);
+
+    public static TimeSpan Ticks(this long value) => TimeSpan.FromTicks(value);
 
     public static string ToDayHHmmss(this TimeSpan timeSpan)
     {
